cee9f743f2e7d9d15a6698b5b97d76e9b894c0c4
Steven Nebo <stv.nebo@gmail.com>
2020-09-07 16:16:19 +0300

    Change form name in form tag

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -87,7 +87,7 @@ const ContactForm = () => {
         }}
       >
         {props => (
-          <Form name="contact-demo" data-netlify={true}>
+          <Form name="pawcar-contact" data-netlify={true}>
             <CustomTextInput label="Name" name="name" type="text" placeholder="" />
             <CustomTextInput label="Email" name="email" type="email" placeholder="" />
             <CustomTextArea label="Message" name="message" placeholder="Type your message here..."/>
