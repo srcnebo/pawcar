2e1cf86dbce18b75c7a5d0616ccd61955ab579d5
Steven Nebo <steven.nebo@tempys-MacBook-Pro.local>
2020-09-07 12:37:46 +0300

    Import bug fix

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,13 +3,13 @@ import {HiOutlineMail} from "react-icons/hi"
 import {BsHouse} from "react-icons/bs"
 import {FaInstagram} from "react-icons/fa"
 
-import footerStyles from "./footer.module.scss"
+import footerStyles from "./Footer.module.scss"
 
 const Footer = () => {
   return (
     <footer className={footerStyles.footer}>
       <div className={footerStyles.contacts}>
-        
+
         <a href="www.instagram.com"><FaInstagram size={20}/> @mypawcar</a>
         <p><BsHouse/> Address goes here</p>
         <p><HiOutlineMail/>  wojcinska.natalia@gmail.com</p>
