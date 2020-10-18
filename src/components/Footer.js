8f62fee9eb19f4663c33f2623c8f4ceb863312fa
Steven Nebo <stv.nebo@gmail.com>
2020-10-18 16:21:09 +0300

    Update footer icons

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,6 +3,13 @@ import { graphql, useStaticQuery } from "gatsby"
 import {AiFillInstagram} from "react-icons/ai"
 
 import footerStyles from "./Footer.module.scss"
+import viber from "../assets/viber.svg"
+import whatsapp from "../assets/whatsapp.svg"
+import flagNg from "../assets/nigeria.svg"
+import flagUk from "../assets/united-kingdom.svg"
+import flagPl from "../assets/poland.svg"
+import fbIcon from "../assets/facebook-color.svg"
+import instaIcon from "../assets/instagram-color.svg"
 
 
 
@@ -10,9 +17,10 @@ const Footer = () => {
   const data = useStaticQuery(graphql`
     query {
       contentfulFooter{
-        address
-        postcode
-        city
+        phoneNg
+        phoneUk
+        phonePl
+        email
       }
     }
   `)
@@ -21,14 +29,28 @@ const Footer = () => {
     <footer className={footerStyles.footer}>
       <div className={footerStyles.footerContainer}>
         <div className={footerStyles.contacts}>
+          
           <p className={footerStyles.contactHeading}>Contact us</p>
-          <p>{data.contentfulFooter.address}</p>
-          <p>{`${data.contentfulFooter.postcode} ${data.contentfulFooter.city}`}</p>
+          <div className={footerStyles.phoneField}>
+            <img src={flagNg} className={footerStyles.footerFlags}/>
+            <p>{data.contentfulFooter.phoneNg}</p>
+          </div>
+          <div className={footerStyles.phoneField}>
+            <img src={flagUk} className={footerStyles.footerFlags}/>
+            <p>{data.contentfulFooter.phoneUk}</p>
+          </div>
+          <div className={footerStyles.phoneField}>
+            <img src={flagPl} className={footerStyles.footerFlags}/>
+            <p>{data.contentfulFooter.phonePl}</p>
+          </div>
           <a href="mailto:o.wojcinski@gmail.com">o.wojcinski@gmail.com</a>
         </div>
         <div className={footerStyles.social}>
-          <p className={footerStyles.contactHeading}>Follow us</p>
-          <a href="https://www.instagram.com/mypawcar" aria-label="instagram" target="_blank" rel="noopener noreferrer"><AiFillInstagram size={36}/></a>
+          <p className={footerStyles.contactHeading}>We are social</p>
+          <a href="https://www.facebook.com/mypawcar" aria-label="instagram" target="_blank" rel="noopener noreferrer"><img src={fbIcon} className={footerStyles.footerIcon}/></a>
+          <a href="https://www.instagram.com/mypawcar" aria-label="instagram" target="_blank" rel="noopener noreferrer"><img src={instaIcon} className={footerStyles.footerIcon}/></a>
+          <a href="#" aria-label="viber" target="_blank" rel="noopener noreferrer"><img src={viber} className={footerStyles.footerIcon}/></a>
+          <a href="#" aria-label="whatsapp" target="_blank" rel="noopener noreferrer"><img src={whatsapp} className={footerStyles.footerIcon}/></a>
         </div>
       </div>
       <div className={footerStyles.credits}>
